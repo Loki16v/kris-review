53ace19c77d7c5685379bf7cd3ab702702640d2e
Ivan Dorosevich <ivan_dorosevich@epam.com>
2024-09-19 02:03:57 +0400

    Added tasks

---
+++ src/tasks_todo/Task2.java
@@ -0,0 +1,17 @@
+package tasks_todo;
+
+/*
+В методе main заполнить массив result элементами из массива array, но обратном порядке.
+Массив result должен быть таким: {25, 49, 93, 23, 78, 44}
+**** Подсказка: в for, в качестве счетчика, можно использовать несколько переменных. Схематичный пример:
+**** for(int i = 0, j = 2; i < array.length; i--, j++) { }
+ */
+
+public class Task2 {
+    private static int[] array = new int[]{44, 78, 23, 93, 49, 25};
+    private static int[] result = new int[array.length];
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/tasks_todo/Task4.java
@@ -0,0 +1,17 @@
+package tasks_todo;
+
+/*
+Вывести в консоль числа, которых нет одновременнно в массивах firstArray и secondArray. Т.е., например число 15 есть
+как в массиве firstArray, так и в secondArray - его выводить не нужно. Число 90 есть только в массиве firstArray, но
+нет в массиве secondArray - число 90 должно быть выведено в консоль. Или число 5, есть в массиве secondArray, но нету
+в массиве firstArray - число 5 должно быть выведено в консоль. Решение должно быть в методе main.
+ */
+
+public class Task4 {
+    private static int[] firstArray = new int[]{15, 67, 32, 90, 53, 94, 19, 38, 25};
+    private static int[] secondArray = new int[]{5, 38, 67, 11, 25, 7, 15, 99, 84, 74};
+
+    public static void main(String[] args) {
+
+    }
+}
